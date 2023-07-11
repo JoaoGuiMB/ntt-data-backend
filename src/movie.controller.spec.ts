9451f5c651ff338a465643b8b6f3625183d39f30
JoaoGuiMB <juaogui159@gmail.com>
2023-07-11 11:46:50 -0300

    test: add movie tests

--- src/movie.controller.spec.ts
+++ src/movie.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MovieController } from './movie.controller';
 import { MovieService } from './movie.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 describe('MovieController', () => {
   let movieController: MovieController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), HttpModule],
       controllers: [MovieController],
       providers: [MovieService],
     }).compile();
@@ -15,8 +18,10 @@ describe('MovieController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(movieController.getMovie('avengers')).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      const movieName = 'the avengers';
+      const res = await movieController.getMovie(movieName);
+      expect(res.title.toLowerCase()).toBe(movieName);
     });
   });
 });
