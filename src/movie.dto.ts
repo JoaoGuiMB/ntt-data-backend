7a7e039b9bbcc0e1bb6e1e25e317677c3b1a47f5
JoaoGuiMB <juaogui159@gmail.com>
2023-07-11 11:38:26 -0300

    fix: assign dto

--- src/movie.dto.ts
+++ src/movie.dto.ts
@@ -1,17 +1,45 @@
-export interface MovieDTO {
+export type MovieDTO = {
+  title: string;
+  year: string;
+  released: string;
+  runtime: string;
+  genre: string;
+  actors: string;
+  plot: string;
+  awards: string;
+  poster: string;
+  imdbRating: string;
+};
+
+export interface MovieResponse {
   Title: string;
   Year: string;
+  Rated: string;
   Released: string;
   Runtime: string;
   Genre: string;
+  Director: string;
+  Writer: string;
   Actors: string;
   Plot: string;
+  Language: string;
+  Country: string;
   Awards: string;
   Poster: string;
+  Ratings: Rating[];
+  Metascore: string;
   imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  Type: string;
+  DVD: string;
+  BoxOffice: string;
+  Production: string;
+  Website: string;
+  Response: string;
 }
 
-export interface Rating {
+interface Rating {
   Source: string;
   Value: string;
 }
--- src/movie.service.ts
+++ src/movie.service.ts
@@ -1,15 +1,27 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { MovieDTO } from './movie.dto';
+import { MovieDTO, MovieResponse } from './movie.dto';
 
 @Injectable()
 export class MovieService {
   constructor(private readonly httpService: HttpService) {}
 
   async getMovie(movieName: string): Promise<MovieDTO> {
-    const { data } = await this.httpService.axiosRef.get<MovieDTO>(
-      `https://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${movieName}`,
+    const { data } = await this.httpService.axiosRef.get<MovieResponse>(
+      `https://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${movieName}&plot=full`,
     );
-    return data;
+    const movie: MovieDTO = {
+      title: data.Title,
+      year: data.Year,
+      actors: data.Actors,
+      awards: data.Awards,
+      genre: data.Genre,
+      plot: data.Plot,
+      poster: data.Poster,
+      released: data.Released,
+      runtime: data.Runtime,
+      imdbRating: data.imdbRating,
+    };
+    return movie;
   }
 }
